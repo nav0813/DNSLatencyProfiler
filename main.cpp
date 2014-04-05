1949a9b7ac32f7f3cdb1457d6e7c8983a6502d1d
Navaneethan Venugopal <nav0813@users.noreply.github.com>
2014-04-04 17:49:14 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,32 @@
+//standard library includes
+#include <string>
 
-#include <iostream>
-#include <sstream>
-
+//Core-classes includes
 #include "Utils.hpp"
 #include "DNSLatencyProfiler.hpp"
 #include "DBHandler.hpp"
 
-using std::cout;
-using std::ostringstream;
-using DNSProfiler::Utils;
-
-
 
 int main(int argc, char* argv[]){
 
+	// local variables to store the parsed program options
 	string sUname, sPasswd, sDBName, sDomainsFile;
 	int iFreq;
-	Utils::vParseOptions(argc, argv, sUname, sPasswd, sDBName, sDomainsFile, iFreq);
-	Utils::m_sCreateLogFileName();
 
+	//parse program options
+	DNSProfiler::Utils::vParseOptions(argc, argv, sUname, sPasswd, sDBName, sDomainsFile, iFreq);
+
+	//create file for logging
+	DNSProfiler::Utils::m_sCreateLogFileName();
+
+	//create DB handler
 	DNSProfiler::DBHandler oDBHandler(sUname,
 									  sPasswd,
 									  sDBName);
 
+	//instantiate the core profiling class
 	DNSProfiler::DNSLatencyProfiler oDNSLatencyProfiler(sDomainsFile, oDBHandler, iFreq);
 
+	//start profiling
 	oDNSLatencyProfiler.m_vStartProfiling();
 }
